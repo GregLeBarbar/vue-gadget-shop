ea91ab5f7451c8b9476c5bd7f0cd25dfeeae12b2
Grégory Charmier <px86dym@etmlnet.local>
2025-02-27 14:44:32 +0100

    step1-solution

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      title: "Vue Gadget Shop",
+      description:
+        "Découvrez les derniers gadgets électroniques de haute technologie !",
+    };
+  },
+});
+
+app.mount("#app");
